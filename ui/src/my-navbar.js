b6da2b27aaf38c9a2fe7ccd274add84f03e86a74
Tamas Piros <2042718+tpiros@users.noreply.github.com>
2024-10-15 15:33:06 +0800

    upload_stream() added based on internal discussions

--- ui/src/my-navbar.js
+++ ui/src/my-navbar.js
@@ -4,12 +4,24 @@ class MyNavbar extends LitElement {
   createRenderRoot() {
     return this;
   }
+
+  toggleMenu(e) {
+    const dropdownMenu = this.querySelector('.dropdown-content');
+    if (dropdownMenu) {
+      dropdownMenu.classList.toggle('hidden');
+    }
+  }
+
   render() {
     return html`
       <div class="navbar bg-base-100">
         <div class="navbar-start">
           <div class="dropdown">
-            <label tabindex="{0}" class="btn btn-ghost lg:hidden">
+            <label
+              tabindex="0"
+              class="btn btn-ghost lg:hidden"
+              @click="${this.toggleMenu}"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-5 w-5"
@@ -26,47 +38,31 @@ class MyNavbar extends LitElement {
               </svg>
             </label>
             <ul
-              tabindex="{0}"
-              class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              tabindex="0"
+              class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 hidden"
             >
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/upload">Upload from browser</a>
-              </li>
-              <li>
-                <a href="/upload-large">Upload large files</a>
-              </li>
+              <li><a href="/">Home</a></li>
+              <li><a href="/upload">Upload from browser</a></li>
+              <li><a href="/upload-large">Upload large files</a></li>
               <li>
                 <a href="/upload-large-stream">Upload large files via stream</a>
               </li>
-              <li>
-                <a href="/gallery">Gallery</a>
-              </li>
+              <li><a href="/gallery">Gallery</a></li>
             </ul>
           </div>
-          <a href="/" class="btn btn-ghost text-xl">
-            Cloudinary Node.js Sample App
-          </a>
+          <a href="/" class="btn btn-ghost text-xl"
+            >Cloudinary Node.js Sample App</a
+          >
         </div>
         <div class="navbar-center hidden lg:flex">
           <ul class="menu menu-horizontal px-1">
+            <li><a href="/">Home</a></li>
+            <li><a href="/upload">Upload from browser</a></li>
+            <li><a href="/upload-large">Upload large files</a></li>
             <li>
-              <a href="/">Home</a>
+              <a href="/upload-large-stream">Upload (large) files via stream</a>
             </li>
-            <li>
-              <a href="/upload">Upload from browser</a>
-            </li>
-            <li>
-              <a href="/upload-large">Upload large files</a>
-            </li>
-            <li>
-              <a href="/upload-large-stream">Upload large files via stream</a>
-            </li>
-            <li>
-              <a href="/gallery">Gallery</a>
-            </li>
+            <li><a href="/gallery">Gallery</a></li>
           </ul>
         </div>
       </div>
